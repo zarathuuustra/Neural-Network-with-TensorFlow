ec037c0fac3db6055d3eaaaa38c9306ef60d1052
Timur B. <T.Berenstein@gmx.de>
2026-05-05 18:38:35 +0200

    Added the backward function to the Square class.

--- TensorSlow/core.py
+++ TensorSlow/core.py
@@ -38,6 +38,10 @@ class Square(Function):
     def forward(self, x):
         return x ** 2
 
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 
 class Exp(Function):
     """
