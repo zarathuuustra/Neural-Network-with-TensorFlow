6e0f6297e28fef6d6d5276827deffddb85f67c63
Timur B. <T.Berenstein@gmx.de>
2026-05-05 18:39:17 +0200

    Added the backward function to the Exp class.

--- TensorSlow/core.py
+++ TensorSlow/core.py
@@ -51,6 +51,10 @@ class Exp(Function):
     def forward(self, x):
         return np.exp(x)
 
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 def numerical_diff(f, x, eps=1e-4):
     """
