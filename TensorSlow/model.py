d912ae1fac81064113b6b2efde24830ea359e9e9
Timur B. <T.Berenstein@gmx.de>
2026-05-08 16:56:32 +0200

    Added the model.py with model classes

---
+++ TensorSlow/model.py
@@ -0,0 +1,28 @@
+import TensorSlow.functions as F
+import TensorSlow.layers as L
+from TensorSlow.layers import Layer
+from TensorSlow import utils
+
+
+class Model(Layer):
+    def plot(self, *inputs, to_file='model.png'):
+        y = self.forward(*inputs)
+        return utils.plot_dot_graph(y, verbose=True, to_file=to_file)
+
+
+class MLP(Model):
+    """Multi-Layer Perceptron."""
+    def __init__(self, fc_output_sizes, activation=F.sigmoid):
+        super().__init__()
+        self.activation = activation
+        self.layers = []
+
+        for i, out_size in enumerate(fc_output_sizes):
+            layer = L.Linear(out_size)
+            setattr(self, 'l' + str(i), layer)
+            self.layers.append(layer)
+
+    def forward(self, x):
+        for l in self.layers[:-1]:
+            x = self.activation(l(x))
+        return self.layers[-1](x)
