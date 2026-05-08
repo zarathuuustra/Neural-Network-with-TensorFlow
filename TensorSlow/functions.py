2bd998e876ac72538370999865c17a5e2864c439
Timur B. <T.Berenstein@gmx.de>
2026-05-08 07:52:52 +0200

    Added functions.py with advanced functions (Sinus, Cosinus, Tanh)

---
+++ TensorSlow/functions.py
@@ -0,0 +1,46 @@
+import numpy as np
+from TensorSlow.core import Function
+
+###### Sinus ################
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * cos(x) # might be problematic
+        return gx
+
+def sin(x):
+    return Sin()(x)
+
+
+######## Cosinus ##############
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+def cos(x):
+    return Cos()(x)
+
+############ Tanh - hyperbolic tangent ###################
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y * y)
+        return gx
+
+
+def tanh(x):
+    return Tanh()(x)
