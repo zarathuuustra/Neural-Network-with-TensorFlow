f11a91d690ed426abd406ca9007e926361a19e43
Timur B. <T.Berenstein@gmx.de>
2026-05-11 10:22:31 +0200

    Added the RNN class in the layers.py

--- TensorSlow/layers.py
+++ TensorSlow/layers.py
@@ -66,6 +66,39 @@ class Linear(Layer):
         y = F.linear(x, self.W, self.b)
         return y
 
+class RNN:
+    def __init__(self, Wx, Wh, b):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+
+    def forward(self, x, h_prev):
+        Wx, Wh, b = self.params
+        t = np.dot(h_prev, Wh) + np.dot(x, Wx) + b
+        h_next = np.tanh(t)
+
+        self.cache = (x, h_prev, h_next)
+        return h_next
+
+    def backward(self, dh_next):
+        Wx, Wh, b = self.params
+        x, h_prev, h_next = self.cache
+
+        dt = dh_next * (1 - h_next ** 2)
+        db = np.sum(dt, axis=0)
+        dWh = np.dot(h_prev.T, dt)
+        dh_prev = np.dot(dt, Wh.T)
+        dWx = np.dot(x.T, dt)
+        dx = np.dot(dt, Wx.T)
+
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+
+        return dx, dh_prev
+
+
+# ---------------- Test ----------------------
 # layer = Layer()
 #
 # layer.p1 = Parameter(np.array(1))
