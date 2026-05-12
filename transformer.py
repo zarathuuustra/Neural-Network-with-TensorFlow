0f0f115fc8b6d6406fbed10b2775a20001738ba1
Timur B. <T.Berenstein@gmx.de>
2026-05-12 11:30:18 +0200

    Added a patch embedding class, the MLP class (multi-layer-perceptron) and the TransformerEncoder class

--- transformer.py
+++ transformer.py
@@ -81,3 +81,64 @@ test_loader = DataLoader(dataset=test_dataset,
 print(f"DataLoader: {train_loader, test_loader}")
 print(f"Length of train_loader: {len(train_loader)} batches of {BATCH_SIZE}...")
 print(f"Length of test_loader: {len(test_loader)} batches of {BATCH_SIZE}...")
+
+# 8. Building Vision Transformer Model from Scratch
+
+class PatchEmbedding(nn.Module):
+    def __init__(self,
+                 img_size,
+                 patch_size,
+                 in_channels,
+                 embed_dim ):
+        super().__init__()
+        self.patch_size = patch_size # # Convolution 2D layer;
+        # kernel= feature detector; stride = steps, how many pixels you want to move over
+        self.proj = nn.Conv2d(in_channels=in_channels,
+                              out_channels=embed_dim,
+                              kernel_size=patch_size,
+                              stride=patch_size)
+        num_patches = (img_size // patch_size) ** 2
+        self.cls_token = nn.Parameter(torch.randn(1,1, embed_dim))
+        # special vector we want to train in the training phase
+        self.pos_embed = nn.Parameter(torch.randn(1,1 + num_patches), embed_dim)
+
+    def forward(self, x: torch.Tensor):
+        B = x.size(0)
+        x = self.proj(x) # (B, E, H/P, W/P)
+        x = x.flatten(2).transpose(1, 2) # to make sure the shapes align -> (B, N, E)
+        cls_token = self.cls_token( B, -1, -1)
+        x = torch.cat((cls_token, x), dim=1)
+        x = x + self.pos_embed
+        return x
+
+class MLP(nn.Module):
+    def __init__(self,
+                 in_features,
+                 hidden_features,
+                 drop_rate):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features=in_features,
+                             out_features=hidden_features)
+        self.fc2 = nn.Linear(in_features=hidden_features,
+                             out_features=in_features)
+        self.dropout = nn.Dropout(drop_rate)
+
+    def forward(self, x): # gelu: activation function
+        x = self.dropout(F.gelu(self.fc1(x)))
+        x = self.dropout(self.fc2(x))
+        return x
+
+class TransformerEncoder (nn.Module):
+    def __init__(self, embed_dim, num_heads, mlp_dim, drop_rate):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=drop_rate, batch_first = True)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.mlp = MLP(embed_dim, mlp_dim, drop_rate)
+
+    def forward (self, x):
+        x = x + self.attn(self.norm1(x), self.norm2(x), self.norm1(x)[0])
+        x = x + self.mlp(self.norm2(x))
+        return x
+
+
