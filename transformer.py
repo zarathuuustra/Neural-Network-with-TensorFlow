5393be435c36d1dcd6e169e60f1f79a6fad91910
Timur B. <T.Berenstein@gmx.de>
2026-05-12 09:40:41 +0200

    I have defined Image Transformations, got a dataset, and converted the datasets into dataloaders

--- transformer.py
+++ transformer.py
@@ -39,3 +39,45 @@ MLP_DIM = 512
 DROP_RATE = 0.1
 
 # 5. Define Image Transformations
+
+transform = transforms.Compose([
+    transforms.ToTensor(), # we need to convert our image to tensors
+    transforms.Normalize((0.5,), (0.5,))
+    # 1. Helps the model to converge faster
+    # 2. Helps to make he numerical computations stable
+])
+
+# 6. Getting a dataset
+
+train_dataset = datasets.CIFAR10(root='data',
+                                 train=True,
+                                 transform=transform,
+                                 download=True)
+
+# print(train_dataset)
+
+test_dataset = datasets.CIFAR10(root='data',
+                                train=False,
+                                download=True,
+                                transform=transform)
+
+# print(test_dataset)
+
+print(len(train_dataset))
+print(len(test_dataset))
+
+# 7.Converting our datasets into dataloaders
+# right now our data is in the form of PyTorch Datasets
+# Dataloader turns our data into batcher or mini-batches
+
+train_loader = DataLoader(dataset=train_dataset,
+                          batch_size=BATCH_SIZE,
+                          shuffle=True)
+
+test_loader = DataLoader(dataset=test_dataset,
+                         batch_size=BATCH_SIZE,
+                         shuffle=False)
+
+print(f"DataLoader: {train_loader, test_loader}")
+print(f"Length of train_loader: {len(train_loader)} batches of {BATCH_SIZE}...")
+print(f"Length of test_loader: {len(test_loader)} batches of {BATCH_SIZE}...")
